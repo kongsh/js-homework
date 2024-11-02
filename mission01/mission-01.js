0ca3d40001fba6178369556b213b1a987faf368c
kongsh <ksh080838@gmail.com>
2024-11-02 11:26:20 +0900

    mission-01.js
    테스트케이스 주석 해제

--- mission01/mission-01.js
+++ mission01/mission-01.js
@@ -22,21 +22,21 @@ function getNumberAtArray(arr, index) {
   return;
 }
 
-// const person = {
-//   name: "Alice",
-//   age: 25,
-//   city: "Wonderland",
-// };
+const person = {
+  name: "Alice",
+  age: 25,
+  city: "Wonderland",
+};
 
-// console.log(getValueAtObject(person, "name")); // 'Alice'
-// console.log(getValueAtObject(person, "age")); // 25
-// console.log(getValueAtObject(person, "city")); // 'Wonderland'
-// console.log(getValueAtObject(person, "country")); // Error !
+console.log(getValueAtObject(person, "name")); // 'Alice'
+console.log(getValueAtObject(person, "age")); // 25
+console.log(getValueAtObject(person, "city")); // 'Wonderland'
+console.log(getValueAtObject(person, "country")); // Error !
 
-// const numbers = [10, 20, 30, 40, 50];
+const numbers = [10, 20, 30, 40, 50];
 
-// console.log(getNumberAtArray(numbers, 2)); // 30
-// console.log(getNumberAtArray("Strong String", 0)); // 30
-// console.log(getNumberAtArray(numbers, 4)); // 50
-// console.log(getNumberAtArray(numbers, 5)); // Error!
-// console.log(getNumberAtArray(numbers, -1)); // Error!
+console.log(getNumberAtArray(numbers, 2)); // 30
+console.log(getNumberAtArray("Strong String", 0)); // Error!
+console.log(getNumberAtArray(numbers, 4)); // 50
+console.log(getNumberAtArray(numbers, 5)); // Error!
+console.log(getNumberAtArray(numbers, -1)); // Error!
